695419a13f013ba8e27ee05d79851164cc80b0f1
patrickbindelli <patrickvenanbindelli@gmail.com>
2022-03-12 15:46:30 -0300

    Initital Commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import CrtScreen from "./CrtScreen/CrtScreen";
+
+function App() {
+  return (
+    <CrtScreen></CrtScreen>
+  );
+}
+
+export default App;
---
+++ src/CrtScreen/CrtScreen.js
@@ -0,0 +1,12 @@
+import './CrtScreen.css';
+
+function CrtScreen() {
+    return (
+      <div className='container'>
+        <textarea className='terminal'></textarea>
+      </div>
+    );
+  }
+  
+  export default CrtScreen;
+  
