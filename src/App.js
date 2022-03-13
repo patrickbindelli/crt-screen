a968cfa9eaac17e3e48dca69354552cdf421041f
patrickbindelli <patrickvenanbindelli@gmail.com>
2022-03-12 22:33:24 -0300

    removed routes

--- src/App.js
+++ src/App.js
@@ -7,19 +7,7 @@ import About from "./Pages/About";
 function App() {
   return (
       <>
-        <Navbar
-            options={[
-              {option:"Home", href: "/"},
-              {option:"Sobre", href: "/sobre"},
-              {option:"Github", href: "https://github.com/patrickbindelli/crt-screen"},
-            ]}
-        />
-        <nav>
-        </nav>
-        <Routes>
-          <Route path="/"/>
-          <Route path="sobre" element={<About />} />
-        </Routes>
+        <About />
       </>
   );
 }
