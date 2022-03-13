e9a16db60049d36ebb4a30263e9cd6d2378efda2
patrickbindelli <patrickvenanbindelli@gmail.com>
2022-03-13 01:49:27 -0300

    rabbit

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -4,35 +4,31 @@ import Typewriter from "typewriter-effect";
 function About(){
 
     return(
-    <div className="about text">
-        <div>
-            <p>Olá, meu nome é Patrick Bindelli</p>    
-            <p>Desenvolvido em React.</p>   
-        </div>     
-        <div className="typewriter">
+    <div className="about text">   
+        <div className="box">
             <Typewriter
-            options={{delay: 30}}
-            onInit={(typewriter)=> {
-            typewriter
-                .pauseFor(2000)
-                .typeString("</Wake up, Neo...>")
-                .pauseFor(1000)
-                .pasteString("<br>")
-                .typeString("</The Matrix has you...>")
-                .pauseFor(1000)
-                .pasteString("<br>")
-                .typeString("</Follow the white rabbit.>")
-                .pauseFor(2000)
-                .pasteString("<br><br>")
-                .typeString("</Knock, knock, Neo.>")
-                .pasteString("<br><br>")
-                .typeString(`(\\ /)`)
-                .pasteString("<br>")
-                .typeString(`( ..)`)
-                .pasteString("<br>")
-                .typeString(`c(")(").`)
-                .start();
-            }}/>
+                options={{delay: 30}}
+                onInit={(typewriter)=> {
+                typewriter
+                    .pauseFor(2000)
+                    .typeString("</Wake up, Neo...>")
+                    .pauseFor(1000)
+                    .pasteString("<br>")
+                    .typeString("</The Matrix has you...>")
+                    .pauseFor(1000)
+                    .pasteString("<br>")
+                    .typeString("</Follow the white rabbit.>")
+                    .pauseFor(2000)
+                    .pasteString("<br><br>")
+                    .typeString("</Knock, knock, Neo.>")
+                    .pasteString("<br><br>")
+                    .typeString(`(\\ /)`)
+                    .pasteString("<br>")
+                    .typeString(`( ..)`)
+                    .pasteString("<br>")
+                    .typeString(`c(")(").`)
+                    .start();
+                }}/>
         </div>
     </div>
     )
